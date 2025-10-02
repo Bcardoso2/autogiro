e1abd2def6f2a58f6c0f155d2dd5ce49b8098d85
Bcardoso2 <38514089+Bcardoso2@users.noreply.github.com>
2025-10-02 19:24:09 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000
 // Middleware
 app.use(cors({
     origin: process.env.NODE_ENV === 'production' 
-        ? 'https://seu-dominio.com' 
+        ? 'https://autogiro.onrender.com' 
         : 'http://localhost:3000',
     credentials: true
 }))
